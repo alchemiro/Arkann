45c113a3c744a2e483634d73ed86029c552da0ec
ghostyboi2793 <73477305+ghostyboi2793@users.noreply.github.com>
2021-10-10 11:46:44 +0300

    Created some activities, view zooms for info on lists/dynamic buttons

---
+++ app/src/main/java/com/example/arkann/EcoCodesList.java
@@ -0,0 +1,13 @@
+package com.example.arkann;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EcoCodesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eco_codes_list);
+    }
+}
