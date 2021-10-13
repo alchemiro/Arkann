6745b93d3125abf501aae2f44a8fc29f0b024a5f
ghostyboi2793 <73477305+ghostyboi2793@users.noreply.github.com>
2021-10-13 10:05:09 +0300

    Beginning eco code list

--- app/src/main/java/com/example/arkann/EcoCodesList.java
+++ app/src/main/java/com/example/arkann/EcoCodesList.java
@@ -2,12 +2,22 @@ package com.example.arkann;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class EcoCodesList extends AppCompatActivity {
+    LinearLayout ll =(LinearLayout) findViewById(R.id.buttonLayout);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eco_codes_list);
+        for(int i=0;i<=499;i++){
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        Button btn=new Button(this);
+        btn.setId(i);
+        }
     }
 }
