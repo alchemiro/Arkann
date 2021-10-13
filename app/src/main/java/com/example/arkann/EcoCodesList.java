2c9a95ddb10242ae326275344a2f72493ba14b4f
amit <ghostyboi2793@gmail.com>
2021-10-13 15:30:15 +0300

    added dynamic button list, need to check (add intents)

--- app/src/main/java/com/example/arkann/EcoCodesList.java
+++ app/src/main/java/com/example/arkann/EcoCodesList.java
@@ -18,6 +18,36 @@ public class EcoCodesList extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT);
         Button btn=new Button(this);
         btn.setId(i);
+        if(i<100){
+            btn.setText("A"+i.toString());
+            if(i%100<10){
+                btn.setText("A0"+i.toString());
+            }
+        }
+        else if(i<200){
+            btn.setText("B"+i.toString());
+            if(i%100<10){
+                btn.setText("B0"+i.toString());
+            }
+        }
+        else if(i<300){
+            btn.setText("C"+i.toString());
+            if(i%100<10){
+                btn.setText("C0"+i.toString());
+            }
+        }
+        else if (i<400){
+            btn.setText("D"+i.toString());
+            if(i%100<10){
+                btn.setText("D0"+i.toString());
+            }
+        }
+        else{
+            btn.setText("E"+i.toString());
+            if(i%100<10){
+                btn.setText("E0"+i.toString());
+            }
+        }
         }
     }
 }
